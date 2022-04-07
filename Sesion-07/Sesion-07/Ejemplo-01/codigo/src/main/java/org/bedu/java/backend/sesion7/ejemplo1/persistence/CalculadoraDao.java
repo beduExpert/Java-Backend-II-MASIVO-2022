d0df0dfa8c868f09c3a6b04999ed5e436fb76eb3
Beto <49173908+beduExpert@users.noreply.github.com>
2022-04-07 10:27:44 -0500

    Delete Sesion-07/Sesion-07 directory

--- Sesion-07/Sesion-07/Ejemplo-01/codigo/src/main/java/org/bedu/java/backend/sesion7/ejemplo1/persistence/CalculadoraDao.java
+++
@@ -1,8 +0,0 @@
-package org.bedu.java.backend.sesion7.ejemplo1.persistence;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface CalculadoraDao {
-    int findValorConstante();
-}
